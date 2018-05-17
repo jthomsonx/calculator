9415d73803327fc8b36ff73e94cd375a164fab42
James Thomson <jthomsonx@gmail.com>
2018-05-17 19:48:16 +0100

    updated calculator functionality

--- script.js
+++ script.js
@@ -3,17 +3,60 @@ const numbers = document.querySelectorAll('.number');
 const operators = document.querySelectorAll('.operator');
 const equals = document.getElementById('equals');
 const clear = document.getElementById('clear');
+var tempArray = [];
+var storeArray = 0; // First string created from tempArray
+var tempOperator = ""; // Operator as chosen on click
+var secondArray; // Second string created from tempArray when 'equals' clicked
+var answer;
 
-numbers.forEach(num => {
-	num.addEventListener("click", function() {
-		display.textContent = num.value;
+// Function to clear the display and all stored values
+function allClear() {
+	clear.addEventListener("click", function() {
+		tempArray = [];
+		storeArray = 0;
+		tempOperator = "";
+		secondArray = 0;
+		answer = 0;
+		display.textContent = 0;
 	});
-});
+};
+allClear();
 
 // Function to update the Display screen when a number is clicked.
-function updateDisplay() {
+function updateDisplay(num) {
+	num.addEventListener("click", function() {
+		tempArray.push(num.value);
+		display.textContent = tempArray.join('');
+	});
+};
+numbers.forEach(updateDisplay);
 
-}
+// Function to store initial value, operator chosen & reset temp Array.
+function storeOperator(op) {
+	op.addEventListener("click", function() {
+		if (storeArray == 0) {
+			storeArray = parseInt(tempArray.join(''));
+		}	
+		tempArray = [];
+		tempOperator = op.value;			
+		console.log(storeArray);
+		console.log(tempArray);
+		console.log(tempOperator);
+	});
+};
+operators.forEach(storeOperator);
+
+// Function to assign Operator, X, & Y, and to compute when equals is pressed.
+function computeAnswer() {
+	equals.addEventListener("click", function() {
+		secondArray = parseInt(tempArray.join(''));
+		console.log(secondArray);
+		console.log(operate(tempOperator, storeArray, secondArray));
+		display.textContent = answer;
+		storeArray = answer; // on completion of equals, store result as first string
+	});
+};
+computeAnswer();
 
 // Initial functions for Add, Subtract, Multiply & Divide
 function add(a, b) { return a + b };
@@ -21,24 +64,19 @@ function subtract(a, b) { return a - b};
 function multiply(a, b) { return a * b};
 function divide(a, b) { return a / b};
 
-console.log(add(6,2));
-console.log(subtract(6,2));
-console.log(multiply(6,2));
-console.log(divide(6,2));
-
 function operate(operator, x, y) {
+	console.log(operator);
+	console.log(x);
+	console.log(y);
 	if (operator == "add") {
-		return add(x, y);
+		answer = add(x, y);
 	} else if (operator == "subtract") {
-		return subtract(x, y);
+		answer = subtract(x, y);
 	} else if (operator == "multiply") {
-		return multiply(x, y);
+		answer = multiply(x, y);
 	} else if (operator == "divide") {
-		return divide(x, y);
+		answer = divide(x, y);
 	}
+	return answer;
 }
-console.log(operate("multiply", 4, 2));
 
-console.log(display.textContent);
-console.log(numbers);
-console.log(clear);
